a35f660af3a56dd1fdfd01e1a4b7ee220ac38c05
Felipe Mendes <felipemadruga@gmail.com>
2023-08-15 19:39:40 -0300

    Conclui função battleship

--- src/batalha_naval.py
+++ src/batalha_naval.py
@@ -1,3 +1,13 @@
 def battleship(grid: list[list[int]], line: int, column: int) -> bool:
     # raise NotImplementedError
-    return
+    if grid[line][column] == 0:
+        return False
+    else:
+        return True
+
+
+# grid = [[0, 0, 0, 0, 1],
+#         [0, 0, 0, 0, 1],
+#         [1, 1, 1, 1, 1],
+#         [0, 0, 0, 1, 0]]
+# print(battleship(grid, 0, 0))
